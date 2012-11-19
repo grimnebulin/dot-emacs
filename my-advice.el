666150a023463edb8df38be7eb7508390e99b140
Sean McAfee <eefacm@gmail.com>
2012-11-18 20:31:26 -0800

    Turns incorrect "called-interactively-p" from variable reference to function call.  D'oh!

--- my-advice.el
+++ my-advice.el
@@ -2,7 +2,7 @@
 
 (defadvice open-line (around vi-style-open-line)
   "Make open-line behave more like vi."
-  (when called-interactively-p
+  (when (called-interactively-p)
     (beginning-of-line)
     ad-do-it
     (unless do-not-indent-after-open-line
@@ -12,34 +12,33 @@
   "*The recenter-proportionally advice will recenter the screen by
 putting the current line this far down the window.")
 
-(defadvice recenter (before recenter-proportionally)
-  (when called-interactively-p
-    (or (ad-get-arg 0)
-        (ad-set-arg 0 (truncate (* *recenter-fraction* (window-body-height)))))))
+(defadvice recenter (before recenter)
+  (or (ad-get-arg 0)
+      (ad-set-arg 0 (truncate (* *recenter-fraction* (window-body-height))))))
 
 (defadvice ucs-insert (before use-ido-completing-read)
   (interactive (list (ido-read-char-by-name "Unicode (name or hex): "))))
 
 (defadvice gnus (after cd-to-home-dir)
-  (when called-interactively-p
+  (when (called-interactively-p)
     (cd "~")))
 
 (defadvice bookmark-jump (around spawn-shell-if-prefix)
   "If a prefix argument is supplied, spawn a shell in the bookmark's directory instead."
-  (when called-interactively-p
+  (when (called-interactively-p)
     (if current-prefix-arg
         (shell-in-bookmark-directory (ad-get-arg 0))
       ad-do-it)))
 
 (defadvice insert-register (before invert-prefix-arg)
   "Invert the sense of the prefix argument to insert-register."
-  (when called-interactively-p
+  (when (called-interactively-p)
     (ad-set-arg 1 (not (ad-get-arg 1)))))
 
 (defadvice dired-do-flagged-delete (around delete-recursively-if-prefix)
   "Sets dired-recursive-deletes to 'always for the duration of this command
 if a prefix argument is present."
-  (when called-interactively-p
+  (when (called-interactively-p)
     (let ((dired-recursive-deletes
            (if current-prefix-arg 'always dired-recursive-deletes)))
       ad-do-it)))
