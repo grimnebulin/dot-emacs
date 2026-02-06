cf9cfaa518902e19c5ea752d10b0bf7f54ad09a0
Sean McAfee <eefacm@gmail.com>
2026-02-05 16:00:13 -1000

    New command toggle-let*

--- functions.el
+++ functions.el
@@ -546,6 +546,17 @@ by using nxml's indentation rules."
                   nconc (dired-get-marked-files nil 'marked))
          (dired-get-marked-files nil (or current-prefix-arg 'marked))))
 
+(defun toggle-let* ()
+  "Make the closest enclosing let or let* form a let* or let form, respectively,
+then reindent that form's bindings."
+  (interactive)
+  (save-excursion
+    (while (progn
+             (condition-case nil (backward-up-list nil t) (scan-error (error "Not within a let form")))
+             (not (search-forward-regexp (rx point "(" (* space) "let" (? "*") symbol-end) nil t))))
+    (if (= ?* (char-before)) (backward-delete-char 1) (insert "*"))
+    (indent-region (point) (progn (forward-sexp) (point)))))
+
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
 ;; End:
